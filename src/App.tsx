1d5d8d72afbae203c3f628e27081ebf26e11c2c8
thasup <thanachonfirst@hotmail.com>
2022-12-05 00:11:52 +0700

    Edit value and unit input

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import ScienceOutlinedIcon from '@mui/icons-material/ScienceOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { FormControl, MenuItem } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 function Copyright(props: any) {
   return (
@@ -28,14 +27,20 @@ function Copyright(props: any) {
 
 const theme = createTheme();
 
-export default function SignUp() {
+export default function App() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      firstValue: data.get('firstValue'),
+      secondValue: data.get('secondValue'),
     });
+  };
+
+  const [unit, setUnit] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setUnit(event.target.value);
   };
 
   return (
@@ -51,33 +56,40 @@ export default function SignUp() {
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
+            <ScienceOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            Unit Converter
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={9}>
                 <TextField
                   autoComplete="given-name"
-                  name="firstName"
+                  name="firstValue"
                   required
                   fullWidth
-                  id="firstName"
-                  label="First Name"
+                  id="firstValue"
+                  label="First Value"
                   autoFocus
                 />
               </Grid>
               <Grid item xs={12} sm={3}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
+                <FormControl sx={{ minWidth: 120 }}>
+                  <Select
+                    value={unit}
+                    onChange={handleChange}
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Without label' }}
+                  >
+                    <MenuItem value="">
+                      <em>Unit</em>
+                    </MenuItem>
+                    <MenuItem value={10}>m</MenuItem>
+                    <MenuItem value={20}>cm</MenuItem>
+                    <MenuItem value={30}>mm</MenuItem>
+                  </Select>
+                </FormControl>
               </Grid>
             </Grid>
           </Box>
